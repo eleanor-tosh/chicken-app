f2bbc485abe7dddad52bf47d5ce924428225c6f6
Eleanor Tosh <eleanortosh@outlook.com>
2023-02-17 18:02:19 +1300

    result page created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Inputs from './pages/Inputs.jsx'
+import Result from './pages/Result.jsx'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/inputs" element={<Inputs />} />
+          <Route path="/result" element={<Result />} />
         </Routes>
       </Router>
     </div>
---
+++ src/pages/Result.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Result() {
+  return (
+    <div class="grid h-screen place-items-center">
+      <h1>The result will be here</h1>
+    </div>
+  )
+}
+
+export default Result
