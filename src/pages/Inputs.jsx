c6a325c2223b38c423fa51e348d8a505dfcd16b3
Eleanor Tosh <eleanortosh@outlook.com>
2023-02-18 15:19:33 +1300

    Form completed

--- src/pages/Inputs.jsx
+++ src/pages/Inputs.jsx
@@ -13,12 +13,43 @@ function Inputs() {
         <h1 className="text-2xl font-bold mb-10 text-center">
           Fill in this form to calculate your backyard hens cost per egg.
         </h1>
+
         <form className="bg-gray-50 shadow-xl w-3/4 mx-auto">
           <div>
+            <div>
+              <div className="text-center col-span-full mb-4">
+                <h2 className="text-xl font-bold underline mb-4">
+                  About the Hens
+                </h2>
+                <p>Give an approximation for the nuumber of eggs.</p>
+              </div>
+              {/* First Other*/}
+              <div className="grid gap-6 mb-6 md:grid-cols-2">
+                <div>
+                  <label className="block mb-1 text-sm font-medium text-gray-900">
+                    Number of Hens
+                  </label>
+                  <input
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 block w-full"
+                    type="number"
+                    placeholder="8"
+                  />
+                </div>
+                <div>
+                  <label className="block mb-1 text-sm font-medium text-gray-900">
+                    Average Eggs per Day
+                  </label>
+                  <input
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 block w-full"
+                    type="number"
+                    placeholder="2"
+                  />
+                </div>
+              </div>
+            </div>
+
             <div className="text-center col-span-full mb-4">
-              <h2 className="text-lg font-semibold underline mb-4">
-                Cost to Feed your Hens
-              </h2>
+              <h2 className="text-xl font-bold underline mb-4">Cost to Feed</h2>
               <p>
                 Don't include kitchen or garden scraps as these are essentially
                 free. Fill in as many rows as needed and leave the rest blank.
@@ -237,6 +268,246 @@ function Inputs() {
                   type="number"
                   placeholder="45.99"
                 />
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div className="text-center col-span-full mb-4">
+              <h2 className="text-xl font-bold underline mb-4">
+                Cost to House
+              </h2>
+              <p>Fill in as many rows as needed and leave the rest blank.</p>
+            </div>
+            {/* First House */}
+            <div className="grid gap-6 mb-6 md:grid-cols-4">
+              <div>
+                <label className="block mb-1 text-sm font-medium text-gray-900">
+                  Name of Bedding
+                </label>
+                <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 block w-full"
+                  type="string"
+                  placeholder="Wood Shavings"
+                />
+              </div>
+              <div>
+                <label className="block mb-1 text-sm font-medium text-gray-900">
+                  Purchase Frequency (days)
+                </label>
+                <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 block w-full"
+                  type="number"
+                  placeholder="120"
+                />
+              </div>
+              <div>
+                <label className="block mb-1 text-sm font-medium text-gray-900">
+                  Size (kgs)
+                </label>
+                <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 block w-full"
+                  type="number"
+                  placeholder="19"
+                />
+              </div>
+              <div>
+                <label className="block mb-1 text-sm font-medium text-gray-900">
+                  Cost ($)
+                </label>
+                <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 block w-full"
+                  type="number"
+                  placeholder="26.95"
+                />
+              </div>
+            </div>
+            {/* Second House */}
+            <div className="grid gap-6 mb-6 md:grid-cols-4">
+              <div>
+                <label className="block mb-1 text-sm font-medium text-gray-900">
+                  Name of Bedding
+                </label>
+                <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 block w-full"
+                  type="string"
+                  placeholder="Wood Shavings"
+                />
+              </div>
+              <div>
+                <label className="block mb-1 text-sm font-medium text-gray-900">
+                  Purchase Frequency (days)
+                </label>
+                <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 block w-full"
+                  type="number"
+                  placeholder="120"
+                />
+              </div>
+              <div>
+                <label className="block mb-1 text-sm font-medium text-gray-900">
+                  Size (kgs)
+                </label>
+                <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 block w-full"
+                  type="number"
+                  placeholder="19"
+                />
+              </div>
+              <div>
+                <label className="block mb-1 text-sm font-medium text-gray-900">
+                  Cost ($)
+                </label>
+                <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 block w-full"
+                  type="number"
+                  placeholder="26.95"
+                />
+              </div>
+            </div>
+            {/* Third House */}
+            <div className="grid gap-6 mb-6 md:grid-cols-4">
+              <div>
+                <label className="block mb-1 text-sm font-medium text-gray-900">
+                  Name of Bedding
+                </label>
+                <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 block w-full"
+                  type="string"
+                  placeholder="Wood Shavings"
+                />
+              </div>
+              <div>
+                <label className="block mb-1 text-sm font-medium text-gray-900">
+                  Purchase Frequency (days)
+                </label>
+                <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 block w-full"
+                  type="number"
+                  placeholder="120"
+                />
+              </div>
+              <div>
+                <label className="block mb-1 text-sm font-medium text-gray-900">
+                  Size (kgs)
+                </label>
+                <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 block w-full"
+                  type="number"
+                  placeholder="19"
+                />
+              </div>
+              <div>
+                <label className="block mb-1 text-sm font-medium text-gray-900">
+                  Cost ($)
+                </label>
+                <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 block w-full"
+                  type="number"
+                  placeholder="26.95"
+                />
+              </div>
+            </div>
+
+            <div className="grid gap-6 mb-6 md:grid-cols-2">
+              <div>
+                <label className="block mb-1 text-sm font-medium text-gray-900">
+                  Cost of Coop ($)
+                </label>
+                <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 block w-full"
+                  type="number"
+                  placeholder="1200"
+                />
+              </div>
+
+              <div>
+                <label className="block mb-1 text-sm font-medium text-gray-900">
+                  Time in Use (years)
+                </label>
+                <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 block w-full"
+                  type="number"
+                  placeholder="5"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="text-center col-span-full mb-4">
+                <h2 className="text-xl font-bold underline mb-4">
+                  Other Costs
+                </h2>
+                <p>
+                  Fill in as many rows as needed and leave the rest blank. There
+                  may be other costs that this form does not take into account.
+                </p>
+              </div>
+              {/* First Other*/}
+              <div className="grid gap-6 mb-6 md:grid-cols-3">
+                <div>
+                  <label className="block mb-1 text-sm font-medium text-gray-900">
+                    Name
+                  </label>
+                  <input
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 block w-full"
+                    type="string"
+                    placeholder="Special Suppliment"
+                  />
+                </div>
+                <div>
+                  <label className="block mb-1 text-sm font-medium text-gray-900">
+                    Purchase Frequency (days)
+                  </label>
+                  <input
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 block w-full"
+                    type="number"
+                    placeholder="365"
+                  />
+                </div>
+                <div>
+                  <label className="block mb-1 text-sm font-medium text-gray-900">
+                    Cost ($)
+                  </label>
+                  <input
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 block w-full"
+                    type="number"
+                    placeholder="60"
+                  />
+                </div>
+              </div>
+              {/* Second Other*/}
+              <div className="grid gap-6 mb-6 md:grid-cols-3">
+                <div>
+                  <label className="block mb-1 text-sm font-medium text-gray-900">
+                    Name
+                  </label>
+                  <input
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 block w-full"
+                    type="string"
+                    placeholder="Special Suppliment"
+                  />
+                </div>
+                <div>
+                  <label className="block mb-1 text-sm font-medium text-gray-900">
+                    Purchase Frequency (days)
+                  </label>
+                  <input
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 block w-full"
+                    type="number"
+                    placeholder="365"
+                  />
+                </div>
+                <div>
+                  <label className="block mb-1 text-sm font-medium text-gray-900">
+                    Cost ($)
+                  </label>
+                  <input
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 block w-full"
+                    type="number"
+                    placeholder="60"
+                  />
+                </div>
               </div>
             </div>
           </div>
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -7,16 +7,16 @@ function Landing() {
       initial={{ opacity: 0, scale: 0.5 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{ duration: 0.5 }}
-      class="grid h-screen place-items-center"
+      className="grid h-screen place-items-center"
     >
-      <div class="relative">
+      <div className="relative">
         <a href="/inputs">
           <img
-            class="max-w-screen-xl hover:opacity-70"
+            className="max-w-screen-xl hover:opacity-70"
             alt="eggs"
             src="/images/eggs.jpg"
           />
-          <div class="absolute text-2xl text-gray-300 bottom-20 left-1/2 -translate-x-1/2">
+          <div className="absolute text-2xl text-gray-300 bottom-20 left-1/2 -translate-x-1/2">
             <p>Click to enter</p>
           </div>
         </a>
