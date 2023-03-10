45420c024e9874bd289877a610518969cb528fbf
Eleanor Tosh <eleanortosh@outlook.com>
2023-03-11 10:53:52 +1300

    Home page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,10 +4,10 @@ function Landing() {
   return (
     <div class="container grid grid-cols-2 mx-auto">
       <div class="p-20">
-        <h1 class="text-5xl pb-10 leading-relaxed font-semibold text-orange-700">
-          Welcome to Scrambled, your backyard chicken website.
+        <h1 class="text-5xl pb-12 leading-relaxed font-semibold text-orange-700">
+          Welcome to <strong>Scrambled</strong>, your backyard chicken website.
         </h1>
-        <p class="text-lg">
+        <p class="text-xl">
           Here you can calculate the cost of each egg that your hens produce,
           find out how much it might cost to set-up a backyard coop, record
           on-going expenses, track your hens, get some handy chicken raising
