3828dc4f85b085e76a726433f19703c8ad92ea67
Eleanor Tosh <eleanortosh@outlook.com>
2023-03-11 14:29:57 +1300

    added some recipes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -48,7 +48,7 @@ function Navbar() {
                   href="/calendar"
                   class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
-                  Calendar
+                  Costs
                 </a>
               </li>
               <li>
--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -4,7 +4,7 @@ function Recipes() {
   return (
     <div class="container p-20 mx-auto ">
       <h1 class="text-2xl pb-12 leading-relaxed font-semibold underline underline-offset-8 text-orange-700">
-        Find some recipes that will use your backyard hens eggs
+        Find some recipes that will use your backyard hens eggs:
       </h1>
       <ul class="text-xl">
         <li>
@@ -12,8 +12,29 @@ function Recipes() {
             href="https://thestayathomechef.com/how-to-boil-eggs/"
             target="_blank"
             rel="noreferrer"
+            class="md:hover:text-green-700 hover:underline"
           >
             How to Boil an Egg
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.delish.com/cooking/recipe-ideas/a23499380/how-to-fry-an-egg/"
+            target="_blank"
+            rel="noreferrer"
+            class="md:hover:text-green-700 hover:underline"
+          >
+            How to Fry an Egg
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.bbcgoodfood.com/recipes/basic-omelette"
+            target="_blank"
+            rel="noreferrer"
+            class="md:hover:text-green-700 hover:underline"
+          >
+            Basic Omelette Recipe
           </a>
         </li>
       </ul>
