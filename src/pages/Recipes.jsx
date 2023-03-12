8adf122f1d5781125d50c9669f3a7a7cb0defb7d
Eleanor Tosh <eleanortosh@outlook.com>
2023-03-12 20:13:00 +1300

    recipe card centering

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -10,24 +10,26 @@ function Recipes() {
       <div class="container grid grid-cols-2 mx-auto mb-20">
         {recipes?.map((recipe) => {
           return (
-            <a
-              href={recipe.link}
-              target="_blank"
-              class="flex flex-col mb-10 items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100"
-              rel="noreferrer"
-            >
-              <img
-                class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                src={recipe.image}
-                alt="completed recipe"
-              />
-              <div class="flex flex-col justify-between p-4 leading-normal">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
-                  {recipe.title}
-                </h5>
-                <p class="mb-3 font-normal text-gray-700">{recipe.comment}</p>
-              </div>
-            </a>
+            <div class="mx-auto">
+              <a
+                href={recipe.link}
+                target="_blank"
+                class="flex flex-col mb-10 items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100"
+                rel="noreferrer"
+              >
+                <img
+                  class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+                  src={recipe.image}
+                  alt="completed recipe"
+                />
+                <div class="flex flex-col justify-between p-4 leading-normal">
+                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                    {recipe.title}
+                  </h5>
+                  <p class="mb-3 font-normal text-gray-700">{recipe.comment}</p>
+                </div>
+              </a>
+            </div>
           )
         })}
       </div>
